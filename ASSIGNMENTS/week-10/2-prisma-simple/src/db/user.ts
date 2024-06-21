0b18b26a0838af132d4b747cb5fcb4ed68d64827
Abhishek Naithani <abhinaithani2@gmail.com>
2024-06-21 22:17:45 +0530

    prisma assignment done

---
+++ ASSIGNMENTS/week-10/2-prisma-simple/src/db/user.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+/*
+ * Should insert into the users table
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function createUser(username: string, password: string, name: string) {
+    const res = await prisma.user.create({
+        data: {
+            username,
+            password,
+            name,
+        },
+        select: {
+            username : true,
+            password : true,
+            name : true,
+            id: true
+        }
+    })
+    return res;
+}
+
+/*
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function getUser(userId: number) {
+    const res = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+        select: {
+            username: true,
+            password: true,
+            name: true,
+            id: true,
+            
+        }
+    })
+    return res;
+}
